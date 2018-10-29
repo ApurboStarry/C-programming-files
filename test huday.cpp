a63a2a14d6977ec729e61b9ca30b7a337c72f788
ApurboStarry <hasinapurbo984@gmail.com>
2018-10-29 22:28:20 +0600

    commit on 29 october

--- test huday.cpp
+++ test huday.cpp
@@ -1,23 +1,30 @@
-#include <bits/stdc++.h>
-#include <math.h>
+#include <iostream>
+#include <algorithm>
 
 using namespace std;
 
-int factorial(int n) {
-    int product = 1;
-    for(int i = n; i >= 1; i--) {
-        product *= i;
-    }
-    return product;
-}
-
-int permutation(int n, int r) {
-    return (factorial(n) / factorial(n-r));
-}
-
 int main()
 {
-    string str = "4456";
-    int a = stoi(str2);
-    cout << a << endl;
+    int n;
+    cin >> n;
+    while(n > 0) {
+        cout << n % 26 << endl;
+        n = n / 26;
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
