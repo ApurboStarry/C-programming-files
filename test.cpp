dad8d6f3053487b46af8ba4eefa18f5f0bc05ef1
ApurboStarry <hasinapurbo984@gmail.com>
2018-11-28 22:05:56 +0600

    commiting

--- test.cpp
+++ test.cpp
@@ -9,7 +9,7 @@ void printSolution(int dist[V][V], int n) {
     cout << "\nAll pair shortest path:" << endl;
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < n; j++) {
-            if(dist[i][j] == 99999)
+            if(dist[i][j] == 999999)
                 cout << "INF" << "\t";
             else
                 cout << dist[i][j] << "\t";
@@ -22,49 +22,22 @@ int summation(int graph[V][V], int n) {
     int sum = 0;
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < n; j++) {
-            if(graph[i][j] != 9999)
+            if(graph[i][j] != 999999)
                 sum += graph[i][j];
         }
     }
     return sum;
 }
 
-int FloydWarshall(int graph[V][V], int n) {
-    int dist[V][V];
+int FloydWarshall(int dist[V][V], int k, int n) {
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < n; j++) {
-            dist[i][j] = graph[i][j];
-        }
-    }
-
-    for(int k = 0; k < n; k++) {
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < n; j++) {
-                if(dist[i][k] + dist[k][j] < dist[i][j])
-                    dist[i][j] = dist[i][k] + dist[k][j];
-            }
+            if(dist[i][k] + dist[k][j] < dist[i][j])
+                dist[i][j] = dist[i][k] + dist[k][j];
         }
     }
     printSolution(dist, n);
     return summation(dist, n);
-
-    /*
-    bool flag;
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < n; j++) {
-            if(dist[i][j] < 0) {
-                flag = true;
-                break;
-            }
-        }
-    }
-
-    cout << "\nNegative Cycle : ";
-    if(flag == true)
-        cout << "YES" << endl;
-    else
-        cout << "NO" << endl;
-    */
 }
 
 int main() {
@@ -87,26 +60,55 @@ int main() {
         nodes[i] = input - 1;
     }
 
+    int dist[V][V];
+    for(int i = 0; i < V; i++) {
+        for(int j = 0; j < V; j++) {
+            dist[i][j] = 999999;
+        }
+    }
+
+    for(int i = n-1; i >= 0; i--) {
+
+    }
+}
+
+/*
+int main() {
+    int graph[V][V];
+    //int originalGraph[V][V];
+    int n, m, u, v, wt;
+    cin >> n;
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            cin >> wt;
+            graph[i][j] = wt;
+            //originalGraph[i][j] = graph[i][j];
+        }
+    }
+
+    int nodes[n];
+    int input;
+
+    for(int i = 0; i < n; i++) {
+        cin >> input;
+        nodes[i] = input - 1;
+    }
+
     int result[n];
 
     //cout << FloydWarshall(graph, n) << endl;
 
 
+
     for(int i = 0; i < n; i++) {
         result[i] = FloydWarshall(graph, n);
-        for(int j = 0; j < n; j++) {
-            graph[nodes[i]][j] = 9999;
-        }
-        for(int j = 0; j < n; j++) {
-            graph[j][nodes[i]] = 9999;
-        }
 
-        cout << endl << "Resultant Graph after removing node " << nodes[i] << endl;
         for(int j = 0; j < n; j++) {
-            for(int k = 0; k < n; k++) {
-                cout << graph[j][k] << " ";
-            }
-            cout << endl;
+            graph[nodes[i]][j] = 999999;
+        }
+        for(int j = 0; j < n; j++) {
+            graph[j][nodes[i]] = 999999;
         }
     }
 
@@ -114,19 +116,6 @@ int main() {
         cout << result[i] << " ";
     }
 
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+*/
